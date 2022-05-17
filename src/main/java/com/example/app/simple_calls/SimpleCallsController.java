ce4a222f0517cbcebb04beafbb0b7c8642823f26
ishiiyuzm <ishiiyuzm0527@gmail.com>
2022-05-17 22:09:21 +0900

    一旦。まとめてビルドはあきらめる

--- src/main/java/com/example/app/simple_calls/SimpleCallsController.java
+++ src/main/java/com/example/app/simple_calls/SimpleCallsController.java
@@ -2,6 +2,8 @@ package com.example.app.simple_calls;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.domain.service.LogService;
@@ -12,6 +14,11 @@ public class SimpleCallsController {
 
     @Autowired
     LogService logService;
+
+    @GetMapping(path = "/")
+    public String getSpa() {
+        return "forward:vue/index.html";
+    }
     
     @RequestMapping("/index")
     public String Index() {
