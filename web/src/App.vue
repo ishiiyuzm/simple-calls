ad9655db14a4ef518bab707e851637bb4692b64f
ishiiyuzm <ishiiyuzm0527@gmail.com>
2022-05-18 22:44:51 +0900

    Vueで実装

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,28 +1,22 @@
 <template>
   <div>
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <video autoplay :srcObject.prop="stream"></video>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'app',
+  data(){
+    return {
+      stream: undefined
+    }
+  },
+  async created() {
+    this.stream = await navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: true
+    })
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
