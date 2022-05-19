7a181ae996a4237c634ee1bd7c692fdab10b5d80
ishiiyuzm <ishiiyuzm0527@gmail.com>
2022-05-19 22:20:32 +0900

    Vueで実装中

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,22 +1,45 @@
 <template>
   <div>
-    <video autoplay :srcObject.prop="stream"></video>
+    <div id="app">
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app',
-  data(){
-    return {
-      stream: undefined
-    }
-  },
-  async created() {
-    this.stream = await navigator.mediaDevices.getUserMedia({
-      video: true,
-      audio: true
-    })
-  }
-}
+  import Vue from 'vue'
+  const app = new Vue({
+      el: '#app',
+      data: function() {
+        return {
+          audios: [],
+          videos: []
+        };
+      },
+      methods: {
+
+      },
+      mounted: async function () {
+        //デバイスへのアクセス
+        const deviceInfos = await navigator.mediaDevices.enumerateDevices();
+        console.log(deviceInfos);
+
+        //1. オーディオデバイスの情報を取得
+        deviceInfos
+        .filter(deviceInfo => deviceInfo.kind === 'audioinput')
+        .map(audio => this.audios.push({text: audio.label || `Microphone ${this.audios.length + 1}`, value: audio.deviceId}));
+
+        //2. カメラの情報を取得
+        deviceInfos
+        .filter(deviceInfo => deviceInfo.kind === 'videoinput')
+        .map(video => this.videos.push({text: video.label || `Camera  ${this.videos.length - 1}`, value: video.deviceId}));
+
+        console.log(this.audios, this.videos); 
+      }
+  })
+
+  app.mount('#app');
 </script>
+
+<style>
+
+</style>
