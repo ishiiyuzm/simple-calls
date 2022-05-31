21a0ee23936d16afdb7b20514402e8e32d20ad8b
ishiiyuzm <ishiiyuzm0527@gmail.com>
2022-05-31 22:58:45 +0900

    カメラ映像の取得方法を変更

--- web/src/components/VideoInfo.vue
+++ web/src/components/VideoInfo.vue
@@ -8,24 +8,6 @@
         <video id="their-video" width="650" autoplay playsinline></video>
       </div>
     </div>
-    <div style="text-align:left">
-      マイク:
-      <select v-model="selectedAudio" @change="onChange">
-        <option disabled value="">マイクを選択してください</option>
-        <option v-for="(audio, key, index) in audios" v-bind:key="index" :value="audio.value">
-          {{ audio.text }}
-        </option>
-      </select>
-    </div>
-    <div style="text-align:left">
-      カメラ: 
-      <select v-model="selectedVideo" @change="onChange">
-        <option disabled value="">カメラを選択してください</option>
-        <option v-for="(video, key, index) in videos" v-bind:key="index" :value="video.value">
-          {{ video.text }}
-        </option>
-      </select>
-    </div>
     <p>あなたの PeerID: <span id="my-id">{{peerId}}</span></p>
     <input v-model="topeerId" placeholder="相手のPeerId">
     &nbsp;
@@ -48,10 +30,6 @@
     name: 'VidepInfo',
     data() {
       return{
-        audios: [],
-        videos: [],
-        selectedAudio: '',
-        selectedVideo: '',  
         peerId: '',
         topeerId: '',
         localStream: {}
@@ -70,38 +48,22 @@
           this.connect(call);
       });
 
-      //デバイスへのアクセス
-      const deviceInfos = await navigator.mediaDevices.enumerateDevices();
-
-      //1. オーディオデバイスの情報を取得
-      deviceInfos
-      .filter(deviceInfo => deviceInfo.kind === 'audioinput')
-      .map(audio => this.audios.push({text: audio.label || `Microphone ${this.audios.length + 1}`, value: audio.deviceId}));
-
-      //2. カメラの情報を取得
-      deviceInfos
-      .filter(deviceInfo => deviceInfo.kind === 'videoinput')
-      .map(video => this.videos.push({text: video.label || `Camera  ${this.videos.length - 1}`, value: video.deviceId}));
-
-      //console.log(this.audios, this.videos); 
+      // カメラ映像取得
+      navigator.mediaDevices.getUserMedia({video: true, audio: true})
+          .then( stream => {
+          // 成功時にvideo要素にカメラ映像をセットし、再生
+          const videoElm = document.getElementById('my-video');
+          videoElm.srcObject = stream;
+          videoElm.play();
+          // 着信時に相手にカメラ映像を返せるように、グローバル変数に保存しておく
+          this.localStream = stream;
+      }).catch( error => {
+          // 失敗時にはエラーログを出力
+          console.error('mediaDevice.getUserMedia() error:', error);
+          return;
+      });
     },
     methods: {
-      onChange: function (){
-          if(this.selectedAudio != '' && this.selectedVideo != ''){
-              this.connectLocalCamera();
-          }
-      },
-      connectLocalCamera: async function (){
-          const constraints = {
-              audio: this.selectedAudio ? { deviceId: { exact: this.selectedAudio } } : false,
-              video: this.selectedVideo ? { deviceId: { exact: this.selectedVideo } } : false
-          }
-
-          const stream = await navigator.mediaDevices.getUserMedia(constraints);
-          document.getElementById('my-video').srcObject = stream;
-
-          this.localStream = stream;
-      },
       makeCall: function(){
         // 接続開始ログを登録
         this.connectInsertLog();
